f57c38bf9e9af591037754ace4149dc1502870e4
Gabbe550 <Gabbegorailla@gmail.com>
2013-11-06 12:11:58 +0100

    Paj lista

---
+++ Galenskap/Program/Paj lista/Paj lista/Paj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paj_lista
+{
+    class Paj
+    {
+        public static int counter { get; set; }
+         public Paj()
+        {
+            counter++;
+        }
+         public string pajnamn { get; set; }
+         public int pajsmak { get; set; }//Från 1 till 10
+         public string pajingred { get; set; }
+
+         public override string ToString()
+         {
+             return "Pajnamn: " + pajnamn + "\npajsmak: " + pajsmak + "\nIngredienser: " + pajingred;
+         }
+    }
+}
---
+++ Galenskap/Program/Paj lista/Paj lista/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paj_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

104f907a98e555fe94d4b17f44e5dc34e11ae767
Gabbe550 <Gabbegorailla@gmail.com>
2014-01-09 08:52:17 +0100

    Galen

---
+++ Galenskap/Program/Uppgiftspapper 002. IO_Users/ConsoleApplication22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // kod 1
+           /* Console.WriteLine("Hej det här är ett meddelande"); // skriver ut ett meddelande
+            Console.WriteLine("Vad heter du?"); 
+
+            string namn = Console.ReadLine(); // personens namn
+
+            Console.WriteLine("Ok " + namn + " Skriv den del av ditt perssonnummer som berättar ditt födelse datum T.ex 10/16 = 16de oktober."); // skriver ut namn och annan text
+            string pn = Console.ReadLine(); // personnummer
+
+            Console.WriteLine("Okej en fråga till vad är din adress du kan vara lugn du är säker...");
+            string add = Console.ReadLine(); // Address
+
+            Console.WriteLine("Ok " + namn + " vi kommer att komma till din adress " + add + " datumet " + pn); // meddelande som innehåller alla inmatningar
+
+            Console.ReadKey();
+            */
+            //kod 2
+            /*  
+            console.WriteLine("Skriv ett ord som du vill lägga till ord till");
+            string ord1 = Console.ReadLine(); // ord1
+            Console.WriteLine("Du skrev " + ord1 + " Skriv ett ord som du vill lägga på"); // skriver ut ord 1
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Du skrev " + ord2 + " Alla ord blir " + ord1 + " " + ord2 + " Skriv ett ord som du vill lägga på"); // skriver ut ord 1 + alla andra ord
+            string ord3 = Console.ReadLine();
+            Console.WriteLine("Du skrev " + ord3 + " Alla ord blir " + ord1 + " " + ord2 + " " + ord3 + " Skriv ett ord som du vill lägga på");
+            string ord4 = Console.ReadLine();
+            Console.WriteLine("Du skrev " + ord4 + " Alla ord blir " + ord1 + " " + ord2 + " " + ord3 + " " + ord4 + " Skriv ett ord som du vill lägga på");
+            string ord5 = Console.ReadLine();
+            Console.WriteLine("Du skrev " + ord5 + " Alla ord blir " + ord1 + " " + ord2 + " " + ord3 + " " + ord4 + " " + ord5 + " Så nu är du klar");
+
+            Console.ReadKey();
+              */
+
+
+        }
+    }
+}
